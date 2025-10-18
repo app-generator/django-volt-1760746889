0715cb7ca7120f8edeb7139878886fbf73ecd127
App Generator <51070104+app-generator@users.noreply.github.com>
2025-10-18 03:21:36 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Campaign(models.Model):
+
+    #__Campaign_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__Campaign_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Campaign")
+        verbose_name_plural = _("Campaign")
+
+
+
+#__MODELS__END
